8a0370d8ab4e07e9d3774357d0f716dbef209c69
ritviksc <contact4ritvik@gmail.com>
2026-07-03 21:40:59 +1200

    Test for process affinity.

---
+++ user/locality.c
@@ -0,0 +1,12 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(){
+    for(int i = 0; i < 5; i++){
+        fork();
+    }
+    // lots of processes competing for harts
+    for(int i = 0; i < 1000000; i++)
+        ;  // busy loop
+    exit(0);
+}
